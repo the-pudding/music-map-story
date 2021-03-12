d4272794a26e67626986145789cb19d5a3eef849
matthew.f.daniels@gmail.com <matthew.f.daniels@gmail.com>
2021-03-12 13:54:40 -0500

    here

--- src/js/main.js
+++ src/js/main.js
@@ -12,6 +12,11 @@ import generateMap from './generateMap.js'
 import choro from './choro.js'
 import monoCulture from './monoCulture'
 
+import "intersection-observer";
+import scrollama from "scrollama";
+
+let scroller = null;
+
 const $body = d3.select('body');
 let previousWidth = 0;
 
@@ -23,6 +28,10 @@ function resize() {
     previousWidth = width;
     graphic.resize();
   }
+  if(scroller){
+    scroller.resize;   
+  }
+  
 }
 
 // function setupStickyHeader() {
@@ -47,6 +56,20 @@ async function init() {
     true // Lazy load the plugin
   );
 
+
+  scroller = scrollama();
+
+  let adjust = d3.scaleLinear().domain([0,1]).range([0,100]);
+  let chartTitle = d3.select(".chart-title")
+  let fixedMap = d3.select(".fixed-map")
+
+
+ 
+  
+
+
+    // setup resize event
+  
   // adds rel="noopener" to all target="_blank" links
   linkFix();
   // add mobile class to body tag
@@ -87,8 +110,11 @@ async function init() {
   // generateMap.init(d3.select(".map-one").node(),[closestLocation.longitude,closestLocation.latitude],"#7f0101",closestLocation.track_name);
   // generateMap.init(d3.select(".map-two").node(),[closestLocation.longitude,closestLocation.latitude],"#017e23",closestDifferent.track_name);
 
-  d3.selectAll(".map-one").select(".headline").style("color","#7f0101").html(`Where &ldquo;${closestLocation.track_name}&rdquo; by ${closestLocation.artist_name} is the most popular song`)
-  d3.selectAll(".map-two").select(".headline").style("color","#017e23").html(`Where &ldquo;${closestDifferent.track_name}&rdquo; by ${closestDifferent.artist_name} is the most popular song`)
+  d3.selectAll(".chart-title").select(".chart-hed").select("span").style("color","#7f0101").html(`Where &ldquo;${closestLocation.track_name}&rdquo; by ${closestLocation.artist_name} is the most popular song`)
+  d3.selectAll(".chart-title").select(".chart-dek").select("span").html(`YouTube Views Jan 15 - Feb 15, 2021`)
+
+
+  
 
   // are you in a monoculture?
 
@@ -141,8 +167,6 @@ async function init() {
 
   let mapCreated = await generateMap.fullMap(d3.selectAll(".map-container").node(),[closestLocation.longitude,closestLocation.latitude],data[0],closestCountry.track_name,choroOutput.colorPallete,choroOutput.filters,8);
 
-
-
   generateMap.filterForSpecific(closestLocation.track_name,"#7f0101")
 
   mapCreated.flyTo({
@@ -167,11 +191,62 @@ async function init() {
     // this animation is considered essential with respect to prefers-reduced-motion
     essential: true
   });
-    
-
-
+  
   d3.selectAll(".diff-country-geo").html(closestCountry.geo_name);
   d3.selectAll(".diff-country-song").html(`&ldquo;${closestCountry.track_name}&rdquo; by ${closestCountry.artist_name}`);
+
+  let labelCrosswalk = {
+    "location-closest": {
+      text: `Where &ldquo;${closestLocation.track_name}&rdquo; by ${closestLocation.artist_name} is the most popular song`,
+      color: '#7f0101',
+      coors: [closestLocation.longitude,closestLocation.latitude],
+      track_name: closestLocation.track_name
+    },
+    "location-diff":{
+      text: `Where &ldquo;${closestDifferent.track_name}&rdquo; by ${closestDifferent.artist_name} is the most popular song`,
+      color: '#017e23',
+      coors: [closestDifferent.longitude,closestDifferent.latitude],
+      track_name: closestDifferent.track_name
+    }
+  }
+
+ // setup the instance, pass callback functions
+ scroller
+  .setup({
+    step: ".step",
+    progress: true,
+    offset: 1,
+  })
+  .onStepProgress((response) => {
+    chartTitle.style("transform",`translate(0,${-(Math.round(adjust(response.progress)))}px`);
+  })
+  .onStepEnter((response) => {
+    let geo = d3.select(response.element).attr("data-geo");
+
+    if(["location-closest","location-diff"].indexOf(d3.select(response.element).attr("data-geo")) > -1){
+      
+      d3.selectAll(".chart-title").select(".chart-hed").select("span").style("color",labelCrosswalk[geo].color).html(labelCrosswalk[geo].text)
+
+      mapCreated.flyTo({
+        center: labelCrosswalk[geo].coors,
+        zoom: 7,
+        bearing: 0,
+        speed: 0.2, // make the flying slow
+        curve: 1, // change the speed at which it zooms out
+        easing: function (t) {
+          return t;
+        },
+        essential: true
+      });
+
+      generateMap.filterForSpecific(labelCrosswalk[geo].track_name,labelCrosswalk[geo].color)
+
+    }
+
+  })
+  .onStepExit((response) => {
+    // { element, index, direction }
+  });
 
 
 
